989cdf3ab3b3869cdc5d1cfbbee2f9cc885123ea
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-10-03 10:26:00 -0700

    Add files via upload

---
+++ MeshForRiverAnimation.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Engine;
+using Unity.Editor;
+using System.Collections;
+
+
+public class MeshClassForRiverMesh { 
+    public MeshXVector Vector3;
+    public MeshYVector Vector3;
+    public MeshZVector Vector3;
+    public float MeshSizeControl = 1000f;
+    public int MaximumMeshSizeHeight = 1000;
+    public int MaximumMeshSizeWidth = 1000;
+
+	void MeshFunction()
+    {
+        MeshXVector = new Vector3(100f, 0.0f, 0.0f);
+        MeshYVector = new Vector3(0.0f, 100f, 0.0f);
+        MeshZVector = new Vector3(0.0f, 0.0f, 100f);
+        MeshFindingFunction = GetComponent<RiverMesh>;
+
+        Vector3 MeshXSize;
+        Vector3 MeshYSize;
+        Vector3 MeshZSize;
+
+
+       
+    }
+	
+}
